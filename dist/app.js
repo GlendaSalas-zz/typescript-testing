0241c85c7433177c7104e7da6cd8311f8a57f459
GlendaSalas <glenda.salas.a@gmail.com>
2021-07-01 11:41:56 -0500

    Add overload functions

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 const e1 = {
     name: 'Max',
     privileges: ['created-server'],
@@ -10,55 +11,15 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployoyeeInformation(emp) {
-    console.log('Name', emp.name);
-    if ('privileges' in emp)
-        console.log('Priviliges', emp.privileges);
-    if ('starDate' in emp)
-        console.log('Star Date', emp.starDate);
-}
-printEmployoyeeInformation(e1);
-class Car {
-    drive() {
-        console.log('DRIVING...');
+const result = add('M', 'A');
+result.split(' ');
+const fetchUserData = {
+    id: 1,
+    name: 'Glenda',
+    job: {
+        title: 'CEO',
+        description: 'this is an important job'
     }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo... ti ', amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck)
-        vehicle.loadCargo(100);
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSped;
-            break;
-    }
-    console.log('Moving with speed', speed);
-}
-moveAnimal({
-    type: 'bird',
-    flyingSpeed: 200,
-});
-const userInput = document.getElementById('u');
-console.log(userInput);
-if (userInput) {
-    userInput.value = "Hi there";
-}
+};
+console.log((_a = fetchUserData === null || fetchUserData === void 0 ? void 0 : fetchUserData.job) === null || _a === void 0 ? void 0 : _a.title);
 //# sourceMappingURL=app.js.map

773c1dd73348cc3f7f45477ab5a93c4884cf34c2
GlendaSalas <glenda.salas.a@gmail.com>
2021-07-06 10:50:12 -0500

    Add constrains type

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
+function merge(ob1, ob2) {
+    return Object.assign(ob1, ob2);
+}
+const mergedObject = merge({ name: 'Glenda' }, { age: 2 });
+console.log(mergedObject);
+function merge2(ob1, ob2) {
+    return Object.assign(ob1, ob2);
+}
+const mergedObject2 = merge({ name: 'Glenda' }, 30);
+console.log(mergedObject);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
 // Generic types
+function merge<T, U>(ob1:T, ob2:U){
+    return Object.assign(ob1, ob2);
+}
+
+const mergedObject = merge({ name: 'Glenda'}, { age: 2})
+console.log(mergedObject)
+
+// type constrains
+
+function merge2<T extends object, U extends object>(ob1:T, ob2:U){
+    return Object.assign(ob1, ob2);
+}
+
+const mergedObject2 = merge({ name: 'Glenda'}, { age: 30 })
+console.log(mergedObject)

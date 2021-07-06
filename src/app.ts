4fc6e3439d1ba03d525f0dda6017470f2b7d5228
GlendaSalas <glenda.salas.a@gmail.com>
2021-07-06 10:59:49 -0500

    Add keyof in generic functions

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,23 @@ function merge2<T extends object, U extends object>(ob1:T, ob2:U){
 }
 
 const mergedObject2 = merge({ name: 'Glenda'}, { age: 30 })
-console.log(mergedObject)
+console.log(mergedObject);
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy> (element: T): [T, string]{
+    let descriptionText = 'Got no value';
+    if (element.length > 0) descriptionText = 'Got ' + element.length + ' values';
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there'));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U){
+    return obj[key];
+}
+
+console.log(extractAndConvert({ name: 'my'}, 'name'));
+
